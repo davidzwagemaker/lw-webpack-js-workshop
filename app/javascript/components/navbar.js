11a75c85dc7031bb878fa11f42c51cc42b7129d8
David Zwagemaker <david@zwagemaker.nl>
2018-12-01 12:28:45 +0000

    added scrolling navbar

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,17 @@
+function initUpdateNavbarOnScroll() {
+  const navbar = document.querySelector('.navbar-wagon');
+  console.log('hello');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight/2) {
+        navbar.classList.add('navbar-wagon-filled');
+        navbar.classList.remove('navbar-wagon-transparent');
+      } else {
+        navbar.classList.remove('navbar-wagon-filled');
+        navbar.classList.add('navbar-wagon-transparent');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
 import 'bootstrap';
 import { loadDynamicBannerText } from '../components/banner';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 const homePage = document.querySelector('.pages.home');
 if (homePage) {
   loadDynamicBannerText();
 }
+
+initUpdateNavbarOnScroll();
